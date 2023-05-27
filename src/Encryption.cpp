e04b8859c0b26cc036e643ff5528b271087668d0
derAndiofficial <a.englert08@gmail.com>
2023-05-27 18:34:23 +0200

    mufzbdtr

--- src/Encryption.cpp
+++ src/Encryption.cpp
@@ -128,11 +128,7 @@ void MultiPassDecrypt (uint8_t *input, uint8_t *output, int passes) {
 
 
 
-/** @param InputString: The unencryptet plain text data to be encrypted
-  * @param Key: The AES256 encryption-key as a Byte[32] array
-  * @param Passes: The number of encryption passes (0 = unencrypted passthrough)
-  * @param return: The encrypted data using multiple AES256 encryption-cycles and random salt
-  */
+
 String Encrypt(String InputString, const uint8_t *Key, int Passes) {
 
   String delimiter = "--";
@@ -191,11 +187,6 @@ String Encrypt(String InputString, const uint8_t *Key, int Passes) {
 }
 
 
-/** @param InputString: The encryptet cypher text data to be decrypted
-  * @param Key: The AES256 decryption-key as a Byte[32] array
-  * @param Passes: The number of decryption passes
-  * @param return: The decrypted data using multiple AES256 decryption-cycles
-  */
 String Decrypt(String InputString, const uint8_t *Key, int Passes)
 {
 
--- src/Encryption.h
+++ src/Encryption.h
@@ -1,3 +1,4 @@
+
 #ifndef Encrypt_AES
 #define Encrypt_AES
 
@@ -5,7 +6,19 @@
 class Encryption {
 public:
 
+/** @param InputString: The unencryptet plain text data to be encrypted
+  * @param Key: The AES256 encryption-key as a Byte[32] array
+  * @param Passes: The number of encryption passes (0 = unencrypted passthrough)
+  * @param return: The encrypted data using multiple AES256 encryption-cycles and random salt
+  */
   String Encrypt(String InputString, const uint8_t *Key, int Passes);
+
+
+  /** @param InputString: The encryptet cypher text data to be decrypted
+  * @param Key: The AES256 decryption-key as a Byte[32] array
+  * @param Passes: The number of decryption passes
+  * @param return: The decrypted data using multiple AES256 decryption-cycles
+  */
   String Decrypt(String InputString, const uint8_t *Key, int Passes);
 
 private:
