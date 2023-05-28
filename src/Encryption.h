af4d1f360bcefda8d1e672acc9d746b98d39f91a
derAndiofficial <a.englert08@gmail.com>
2023-05-29 00:22:55 +0200

    -.o,kimjunzhbfgvdf

--- src/Encryption.h
+++ src/Encryption.h
@@ -11,13 +11,13 @@ public:
   void setSecrets (const uint8_t *Key);
 
 
-/** @param InputString: The unencryptet plain text data to be encrypted (MUST NOT CONTAIN '!')
+/** @param InputString: The unencrypted plain text data to be encrypted (MUST NOT CONTAIN '!')
   * @param return: The encrypted data using AES256 encryption and random salt
   */
   String Encrypt(String InputString/*, const uint8_t *Key, int Passes*/);
 
 
-/** @param InputString: The encryptet cypher text data to be decrypted
+/** @param InputString: The encrypted cypher text data to be decrypted
   * @param return: The decrypted data using AES256 decryption
   */
   String Decrypt(String InputString/*, const uint8_t *Key, int Passes*/);
